8888bf70cfd78e0a0f5f2492f065056f8e1d07b5
Valentin Vogel <vv@move-elevator.de>
2016-11-14 14:09:02 +0100

    add events

---
+++ Events.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace humhub\modules\humhubModuleExample;
+
+use Yii;
+use yii\helpers\Url;
+use yii\base\Object;
+use humhub\modules\humhubModuleExample\widgets\BaseWidget;
+
+/**
+ * Class Events
+ *
+ * @package humhub\modules\humhubModuleExample
+ */
+class Events extends Object
+{
+    /**
+     * @param $event
+     */
+    public static function onTopMenuInit($event)
+    {
+        if (Yii::$app->user->isGuest) {
+            return;
+        }
+
+        $event->sender->addItem(array(
+            'label' => Yii::t('HumHubModuleExampleModule.base', 'topMenuLabel'),
+            'url' => Url::to(['/humhubModuleExample/base/index']),
+            'icon' => '<i class="fa fa-bug"></i>',
+            'isActive' => (
+                Yii::$app->controller->module
+                && Yii::$app->controller->module->id == 'humhubModuleExample'
+                && Yii::$app->controller->id == 'base'
+            ),
+            'sortOrder' => 430,
+        ));
+    }
+
+    /**
+     * @param $event
+     */
+    public static function onDashboardSidebarInit($event)
+    {
+        $event->sender->addWidget(BaseWidget::className(), array(), array('sortOrder' => -1));
+    }
+}
